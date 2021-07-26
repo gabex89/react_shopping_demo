c860d9c351a27f277e4c2d8e719b70c2eec852c8
Gábor, Kállay <kallay.gabesz@gmail.com>
2021-07-26 16:50:09 +0200

    shop page and preview item

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React from 'react';
-import './App.css';
-import HomePage from './pages/homepage/homepage.component';
 import { Switch, Route } from 'react-router-dom';
+
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
+
+import './App.css';
 
 const HatsPage = () => (
   <div>
@@ -14,7 +17,7 @@ function App() {
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route path="/shop/hats" component={HatsPage} />
+        <Route path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
